36087e2cf1dcc4998dc435bd4178d03845f05ac4
Ahmed Ali <aali.97@yahoo.com>
2023-03-02 13:59:23 +0500

    first commit

---
+++ TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureApp.swift
+//  TheComposableArchitecture
+//
+//  Created by Ahmed Ali on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
